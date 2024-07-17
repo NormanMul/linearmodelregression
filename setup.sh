07fab50965e42c175f268c3c551ecb9eb623f978
Naufal Prawironegoro <56763735+NormanMul@users.noreply.github.com>
2024-07-17 23:48:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
+# Update pip
 pip install --upgrade pip
 
-# Install NumPy and Pandas first, with specific versions
-pip install numpy==1.24.3 pandas==1.5.3
-
-# Install other libraries
-pip install scikit-learn joblib
+# Install necessary libraries using pip
+pip install pandas numpy scikit-learn joblib
