8d51682a323c0d0db32abfdafbf579d9520b9c08
Naufal Prawironegoro <56763735+NormanMul@users.noreply.github.com>
2024-07-17 23:44:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 pip install --upgrade pip
 
- # Install libraries from requirements.txt, excluding NumPy
- pip install --no-deps -r requirements.txt
+# Install NumPy and Pandas first, with specific versions
+pip install numpy==1.24.3 pandas==1.5.3
 
- # Install NumPy from the wheel file (Replace 'numpy‑1.24.3‑cp311‑cp311‑manylinux_2_17_x86_64.manylinux2014_x86_64.whl' with the actual filename)
- pip install numpy‑1.24.3‑cp311‑cp311‑manylinux_2_17_x86_64.manylinux2014_x86_64.whl 
+# Install other libraries
+pip install scikit-learn joblib
